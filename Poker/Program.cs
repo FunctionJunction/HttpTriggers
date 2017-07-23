adf7974b1745bb09fb406cf6f74aaea60620c00d
Simon Timms <stimms@gmail.com>
2017-07-22 22:12:12 -0600

    Adding service bus trigger

--- Poker/Program.cs
+++ Poker/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Common;
+using Microsoft.ServiceBus.Messaging;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +18,7 @@ namespace Poker
         {
             Console.WriteLine("What sort of message would you like to send?");
             Console.WriteLine("1. Storage queue");
+            Console.WriteLine("2. Service Bus queue");
             Console.WriteLine("x. Exit");
 
             while (true)
@@ -23,9 +26,9 @@ namespace Poker
                 var line = Console.ReadLine();
                 if (line == "1")
                 {
-                    var storageAccount = CloudStorageAccount.Parse(System.Configuration.ConfigurationManager.AppSettings["storageConnectionString"]);
+                    var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["storageConnectionString"]);
                     var queueClient = storageAccount.CreateCloudQueueClient();
-                    var queueReference = queueClient.GetQueueReference(System.Configuration.ConfigurationManager.AppSettings["storageQueue"]);
+                    var queueReference = queueClient.GetQueueReference(ConfigurationManager.AppSettings["storageQueue"]);
                     queueReference.CreateIfNotExists();
                     queueReference.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(new SendEmailMessage
                     {
@@ -34,6 +37,19 @@ namespace Poker
                         DestinationAddress = "fans@functionjunction.com"
                     })));
                 }
+                if (line == "2")
+                {
+                    var client = QueueClient.CreateFromConnectionString(ConfigurationManager.AppSettings["serviceBusAccount"],
+                        ConfigurationManager.AppSettings["storageQueue"]);
+                    client.Send(new BrokeredMessage(new SendEmailMessage
+                    {
+                        Subject = "Hello Function Junctioners",
+                        Body = "Welcome to Function Junction",
+                        DestinationAddress = "fans@functionjunction.com"
+                    }));
+                }
+                if (line == "x")
+                    return;
             }
         }
     }
--- Triggers/StorageQueue/QueueTrigger.cs
+++ Triggers/StorageQueue/QueueTrigger.cs
@@ -9,10 +9,19 @@ namespace Triggers.StorageQueue
     public static class QueueTrigger
     {
         [FunctionName("SendEmail")]        
-        public static void Run([QueueTrigger("emailoutput", Connection = "developmentStorage")]string queueMessage, TraceWriter log)
+        public static void SendEmailWithStorageQueues([QueueTrigger("emailoutput", Connection = "developmentStorage")]string queueMessage, TraceWriter log)
         {
             log.Info($"C# Queue trigger function processed: {queueMessage}");
             var message = JsonConvert.DeserializeObject<SendEmailMessage>(queueMessage);
+            //Go send e-mail
+        }
+
+        [FunctionName("SendEmail2")]
+        public static void SendEmailWithServiceBusQueue([ServiceBusTrigger("emailoutput", Connection = "serviceBusAccount")]SendEmailMessage queueMessage, TraceWriter log)
+        {
+            log.Info($"C# Service Bus trigger function processed: {queueMessage}");
+            
+            //Go send e-mail
         }
     }
 }
